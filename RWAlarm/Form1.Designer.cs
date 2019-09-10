a073ee516417bb5711149cdf49e8224c96d8f2d4
Hong <procyonx@member.sdu.ac.kr>
2019-09-10 10:39:46 +0900

    로직 추가

--- RWAlarm/Form1.Designer.cs
+++ RWAlarm/Form1.Designer.cs
@@ -29,34 +29,48 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(92, 51);
+            this.button1.Location = new System.Drawing.Point(122, 47);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(113, 40);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(35, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(65, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "100";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(369, 341);
+            this.ClientSize = new System.Drawing.Size(247, 151);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- RWAlarm/Form1.cs
+++ RWAlarm/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -18,7 +19,27 @@ namespace RWAlarm
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Test");
+            if(!Directory.Exists(@"c:\_a"))
+            {
+                Directory.CreateDirectory(@"c:\_a");
+            }
+
+            int count = 0;
+            if( int.TryParse(textBox1.Text, out count))
+            {
+                for (int i = 1; i <= count; i++)
+                {
+                    string data = Guid.NewGuid().ToString();
+                    File.WriteAllText(@"C:\_a\File_" + i.ToString("00000") + ".txt", data);
+                }
+
+                MessageBox.Show("OK");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
